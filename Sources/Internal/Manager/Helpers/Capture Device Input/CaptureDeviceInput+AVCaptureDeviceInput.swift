5b71ec0556f714753192c87a18d54cb7b180bbaa
Jan Nemeček <jan@jannemecek.com>
2024-12-17 13:02:42 +0100

    Use device type discovery when setting up AVCaptureDeviceInput

--- Sources/Internal/Manager/Helpers/Capture Device Input/CaptureDeviceInput+AVCaptureDeviceInput.swift
+++ Sources/Internal/Manager/Helpers/Capture Device Input/CaptureDeviceInput+AVCaptureDeviceInput.swift
@@ -13,14 +13,38 @@ import AVKit
 
 extension AVCaptureDeviceInput: CaptureDeviceInput {
     static func get(mediaType: AVMediaType, position: AVCaptureDevice.Position?) -> Self? {
-        let device = { switch mediaType {
-            case .audio: AVCaptureDevice.default(for: .audio)
-            case .video where position == .front: AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)
-            case .video where position == .back: AVCaptureDevice.default(for: .video)
-            default: fatalError()
-        }}()
+        let device = {
+            switch mediaType {
+            case .audio:
+                return AVCaptureDevice.default(for: .audio)
+            case .video where position == .front:
+                let devices = videoDeviceType(mediaType: mediaType, position: position!)
+                return AVCaptureDevice.default(devices.first ?? .builtInWideAngleCamera, for: .video, position: .front)
+            case .video where position == .back:
+                let devices = videoDeviceType(mediaType: mediaType, position: position!)
+                return AVCaptureDevice.default(devices.first ?? .builtInWideAngleCamera, for: .video, position: .back)
+            default:
+                fatalError()
+            }
+        }()
 
         guard let device, let deviceInput = try? Self(device: device) else { return nil }
         return deviceInput
+    }
+    
+    private static func videoDeviceType(mediaType: AVMediaType, position: AVCaptureDevice.Position) -> [AVCaptureDevice.DeviceType] {
+        let discoverySession = AVCaptureDevice.DiscoverySession(
+            // TODO: Extract preferred device types to be a public API
+            deviceTypes: [.builtInTripleCamera, .builtInDualCamera, .builtInWideAngleCamera],
+            mediaType: mediaType,
+            position: position
+        )
+        let devices = discoverySession.devices
+        
+        print("Available camera devices:")
+        for device in devices {
+            print("Device name: \(device.localizedName)")
+        }
+        return devices.map { $0.deviceType }
     }
 }
